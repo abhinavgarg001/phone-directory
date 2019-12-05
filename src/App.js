925c5b5719129063e31ebd48c77425c821049bc0
abhinavgarg001 <abhinavgarg616@gmail.com>
2019-12-05 10:03:28 +0530

    JSX vs. HTML: Created Skeleton of Home Page

--- src/App.js
+++ src/App.js
@@ -4,16 +4,14 @@ import React from 'react';
 function App() {
   return (
    <div>
-     Phone Directory
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+            <div className="header">
+          Phone Directory
+        </div>
+        <button>Add</button>
+        <div>
+          <span>Name</span><br />
+          <span>Phone</span>
+        </div>
     </div>
   );
 }
